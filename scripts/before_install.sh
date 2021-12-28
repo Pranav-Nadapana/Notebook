8c0cd4e90c5bface4c1a57293a9d9a25cb2e5f10
Pranav <nadapana1pranav@gmail.com>
2021-12-29 04:20:50 +0530

    adding appspec.yml

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt-get update -y
-sudo apt-get install -y openjdk-8-jdk vim git tree 
 sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo bash - 
 sudo apt install nodejs -y 
 sudo npm install -g yarn 
