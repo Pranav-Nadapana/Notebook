b311027eb7ceb62e0438bc547a884c1a2e27d488
Pranav <nadapana1pranav@gmail.com>
2021-12-30 00:58:53 +0530

    after install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/app
 sudo yarn install
+sudo yarn build
+sudo yarn global add serve
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/app
-forever start -c "npm start" ./ 
+serve -s build
